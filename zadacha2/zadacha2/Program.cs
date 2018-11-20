278eac4f8c640707aeb485f6cc7606cc598ce075
KhalyavinSergey <44732519+KhalyavinSergey@users.noreply.github.com>
2018-11-20 19:42:59 +0300

    Add files via upload

---
+++ zadacha2/zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacha2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1; 
+            
+            double s = 0; 
+            for (int i = 1; i <= n; i++)
+            { 
+                s += 5 / (25 * (Math.Pow(n, 2)) - 5 * n - 6);
+                
+
+                }
+                Console.WriteLine("Sum = {0}", s);
+                Console.ReadLine();
+
+            }
+        }
+    }
+
+   
